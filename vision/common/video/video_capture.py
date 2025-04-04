55a34d68b85ad2b3ac6856ba1bc673d91d112f2d
Iracema <110850107+Iracema008@users.noreply.github.com>
2025-04-03 17:12:53 -0700

    Depthai and cv2 mode switch

--- vision/common/video/video_capture.py
+++ vision/common/video/video_capture.py
@@ -1,9 +1,11 @@
 """Module for video capture."""
 
 import cv2
+import depthai as dai
 import numpy
 import queue
 import threading
+
 from types import SimpleNamespace
 from typing import Union
 
@@ -34,33 +36,77 @@ class VideoCapture:
         Raises:
             RunTimeError: Failed to initialize Video Capture
         """
-        self.video_cap: cv2.VideoCapture = cv2.VideoCapture(video_conf.source_idx)
+        
 
-        if self.video_cap is None or not self.video_cap.isOpened():
-            raise RuntimeError("Failed to initialize Video Capture. Try a different index.")
+        # TODO: Integrate depthai frame capture with current opencv implementation
+        self.use_depthai = getattr(video_conf, "use_depthai", False)
 
-        self.video_cap.set(cv2.CAP_PROP_FRAME_WIDTH, video_conf.width)
-        self.video_cap.set(cv2.CAP_PROP_FRAME_HEIGHT, video_conf.height)
+
+        if self.use_depthai:
+            # Create pipeline (graph of nodes)
+            pipeline = dai.Pipeline()
+
+            # Creating camera nodes
+            camera_rgb = pipeline.create(dai.node.ColorCamera)
+            x_out = pipeline.create(dai.node.XLinkOut)
+
+            x_out.setStreamName("rgb")
+            camera_rgb.setPreviewSize(300,300)
+            camera_rgb.getInterleaved(False)
+            camera_rgb.setColorOrder(dai.ColorCameraProperties.ColorOrder.RGB)
+
+            # Linking output and input stream
+            camera_rgb.preview.link(x_out.input)
+            
+            with dai.Device(pipeline) as self.device:  
+                # Output queue will be used to get the rgb frames from the output defined above
+                self.queue_rgb = self.device.getOutputQueue("rgb")
+                logger.info("Initialized Depthai pipeline")
+        else:
+            #frame = queue_rfgb.get()
+            #print ("tryna open camera")
+            #print(cv2.getBuildInformation()
+            self.video_cap: cv2.VideoCapture = cv2.VideoCapture(0)    
+         
+            if self.video_cap is None or not self.video_cap.isOpened():
+                raise RuntimeError("Failed to initialize ideo Capture. Try a different index.")
+            
+            self.video_cap.set(cv2.CAP_PROP_FRAME_WIDTH, video_conf.width)
+            self.video_cap.set(cv2.CAP_PROP_FRAME_HEIGHT, video_conf.height)
+
         self.frame_buffer: queue.Queue = queue.Queue(maxsize=video_conf.max_buffer_size)
         self.stop_event: threading.Event = threading.Event()
 
     def start(self) -> None:
         """Starts the video capture thread."""
         logger.info("Starting video capture thread")
-        self.capture_thread = threading.Thread(target=self._capture_frames)
-        self.capture_thread.start()
+        try: 
+            self.capture_thread = threading.Thread(target=self._capture_frames)
+            self.capture_thread.start()
+            logger.info("Threading starts successfully")
+        except Exception as e:
+            logger.error(f"Error starting thread: {e}")
 
     def _capture_frames(self) -> None:
         """Capture frames from the camera and put it in the buffer."""
-        while not self.stop_event.is_set():
-            # ret: bool, frame: numpy.ndarray
-            ret, frame = self.video_cap.read()
-            if not ret:
-                break
-            # only populate to frame buffer if there is available space
-            if not self.frame_buffer.full():
-                self.frame_buffer.put(frame)
-        self.video_cap.release()
+        if self.use_depthai:
+            pass
+        else:
+            while not self.stop_event.is_set():
+                # ret: bool, frame: numpy.ndarray
+                # read function is returning image into"frame" 
+                # if no frames are grabbed, will be empty 
+                ret, frame = self.video_cap.read()
+                print(f"frame read ")
+                
+                if not ret:
+                    logger.error("Failed to read frame from capture")
+                    break
+                # only populate to frame buffer if there is available space
+                if not self.frame_buffer.full():
+                    
+                    self.frame_buffer.put(frame)
+            self.video_cap.release()
 
     def stop(self) -> None:
         """Stop the video capture thread."""
